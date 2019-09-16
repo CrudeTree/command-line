06217d2997f1104542a350357ec691f4c74f24ce
James Larivee <mr_15th_entertainer@yahoo.com>
2019-09-16 11:37:39 -0600

    Changed upper case to lower case.

--- src/edu/cnm/deepdive/CommandLineArgs.java
+++ src/edu/cnm/deepdive/CommandLineArgs.java
@@ -6,12 +6,9 @@ public class CommandLineArgs {
 
   public static void main(String[] args) {
     for (String arg : args) {
-      System.out.println(arg.toUpperCase());
-
+      System.out.println(arg.toLowerCase());
     }
-
     System.out.println(Arrays.toString(args));
-
   }
 
 }
